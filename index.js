16bff4c95e420f928ac9509e85d931b94109bc39
Joe Karasek <joseph.karasek@gmail.com>
2017-03-08 09:36:50 -0800

    toggle on browswerSync

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const browserSync = require('browser-sync');
 
 module.exports = (gulp, config, tasks) => {
 
-  // if (config.browserSync.enabled) {
-  //   require('./browserSync')(gulp, config, tasks);
-  // }
+  if (config.browserSync.enabled) {
+    require('./lib/browserSync')(gulp, config, tasks);
+  }
 
   if (config.css.enabled) {
     require('./lib/css')(gulp, config, tasks);
